a2967738edf5c4aca668829b99219d7f892c3baf
iMatias-ED <matdj31@gmail.com>
2022-10-03 17:22:28 -0300

    Added content

--- src/app/sections/components/projects-section/projects-section.component.ts
+++ src/app/sections/components/projects-section/projects-section.component.ts
@@ -17,9 +17,12 @@ export class ProjectsSectionComponent implements AfterViewInit {
   objects!: HTMLCollectionOf<Element>
 
   projectsInfo: ProjectInfo[] = [
-    { icon: ['fab', 'angular'], title: "Project", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro quis laborum vel, impedit nemo, dolor iusto quibusdam error fugiat voluptate obcaecati maxime ratione eaque, molestias alias facere voluptatum quo culpa." },
-    { icon: ['fab', 'python'], title: "Project", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro quis laborum vel, impedit nemo, dolor iusto quibusdam error fugiat voluptate obcaecati maxime ratione eaque, molestias alias facere voluptatum quo culpa." },
-    { icon: ['fab', 'python'], title: "Project", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro quis laborum vel, impedit nemo, dolor iusto quibusdam error fugiat voluptate obcaecati maxime ratione eaque, molestias alias facere voluptatum quo culpa." },
+    { icon: ['fab', 'angular'], title: "Loan Calculator", 
+      description: "  Built in Angular, the loan calculator is part of a local cooperative's user-facing suite." },
+    { icon: ['fab', 'python'], title: "Gym Control Software", 
+      description: "  Built in Python, using PySide6 and SQLite3, this is a custom software created to administrate a small local gym business." },
+    { icon: ['fab', 'wordpress'], title: "\"El país que soñamos\"", 
+      description: "Built in wordpress for an ORG organization, this is a simple website where an annual magazine, created by the community, is published." },
   ]
 
   constructor() { }
@@ -27,21 +30,17 @@ export class ProjectsSectionComponent implements AfterViewInit {
   ngAfterViewInit(): void {
     this.objects = document.getElementsByClassName("project-info")
     this.observer = new IntersectionObserver( this.animaObjeto, { 
-      root: null, rootMargin: "1000px 0px 150px 0px", threshold: 1.0 } )
+      root: null, rootMargin: "1000px 0px 200px 0px", threshold: 1.0 } )
 
     for ( let index of this.range( this.objects.length ) )
       this.observer.observe( this.objects.item(index)! )
   }
 
   animaObjeto( entries: IntersectionObserverEntry[], observer: IntersectionObserver ) {
-    
     for ( let entry of entries ) {
-      if ( entry.isIntersecting ) {
-        entry.target.classList.add("visible")
-      } 
+      if ( entry.isIntersecting ) entry.target.classList.add("visible")
       else entry.target.classList.remove("visible")
     }
-
   }
 
   range( value: number ) {
