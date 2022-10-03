db9daad07306dc5fb06fb2d77871e903c33cc992
iMatias-ED <matdj31@gmail.com>
2022-10-03 10:59:47 -0300

    - Mobile bugs fixed (overflow-x was breaking the screen) - Some responsive modifications

--- src/app/sections/components/projects-section/projects-section.component.ts
+++ src/app/sections/components/projects-section/projects-section.component.ts
@@ -1,17 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+
+interface ProjectInfo {
+  icon: IconProp;
+  title: string;
+  description: string;
+}
 
 @Component({
   selector: 'projects-section',
   templateUrl: './projects-section.component.html',
   styleUrls: ['./projects-section.component.scss']
 })
-export class ProjectsSectionComponent implements OnInit {
+export class ProjectsSectionComponent implements AfterViewInit {
   observer!: IntersectionObserver;
   objects!: HTMLCollectionOf<Element>
 
+  projectsInfo: ProjectInfo[] = [
+    { icon: ['fab', 'angular'], title: "Project", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro quis laborum vel, impedit nemo, dolor iusto quibusdam error fugiat voluptate obcaecati maxime ratione eaque, molestias alias facere voluptatum quo culpa." },
+    { icon: ['fab', 'python'], title: "Project", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro quis laborum vel, impedit nemo, dolor iusto quibusdam error fugiat voluptate obcaecati maxime ratione eaque, molestias alias facere voluptatum quo culpa." },
+    { icon: ['fab', 'python'], title: "Project", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro quis laborum vel, impedit nemo, dolor iusto quibusdam error fugiat voluptate obcaecati maxime ratione eaque, molestias alias facere voluptatum quo culpa." },
+  ]
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     this.objects = document.getElementsByClassName("project-info")
     this.observer = new IntersectionObserver( this.animaObjeto, { 
       root: null, rootMargin: "1000px 0px 150px 0px", threshold: 1.0 } )
